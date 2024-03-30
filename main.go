75e29d32220b66b340de5eaf3b8e00f3651f639b
Bhaskar Mahajan <bmahajan@zscaler.com>
2024-03-31 03:06:24 +0530

    graceful shutdown complete

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/mbhaskar98/orders-api/application"
 )
 
 func main() {
-	err := application.New().Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	err := application.New().Start(ctx)
 
 	if err != nil {
 		fmt.Println("Error occured while starting server:", err)
